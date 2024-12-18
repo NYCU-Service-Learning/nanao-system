d9f3542ba471b49300fb12b93c43f4f9db2187a4
userwei <70355885+chou-ting-wei@users.noreply.github.com>
2024-12-18 15:58:17 +0800

    fix(feature/AI_Avatar): fix file places and bugs in frontend and backend

--- pushImage.sh
+++ pushImage.sh
@@ -2,5 +2,6 @@ docker-compose build
 
 docker push userwei/nycu_service-learning-nanao:backend
 docker push userwei/nycu_service-learning-nanao:frontend
+docker push userwei/nycu_service-learning-nanao:backend_avatar
 
 docker-compose up -d
