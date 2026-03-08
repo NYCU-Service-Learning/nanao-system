a587297afc524457aeba4604016719fcadad9f14
Wither37 <eason37.cs12@nycu.edu.tw>
2026-03-08 22:12:39 +0800

    Fix: linting

--- frontend/src/assets/Interact/DataFiller.tsx
+++ frontend/src/assets/Interact/DataFiller.tsx
@@ -1,20 +1,21 @@
 import { Modal, Radio, Slider, Form, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { RadioChangeEvent } from "antd/lib/radio";
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from "react-router-dom";
 
 // 定義 DataFillerProps 介面，包含組件需要的屬性，主要是當前部位和疼痛狀態的設定函數
 interface DataFillerProps {
-  currentPart: string;  // 當前選擇的身體部位 ID
-  setCurrentPart: (part: string) => void;  // 更新當前部位的函數
-  PainLevel: { [key: string]: number };  // 每個部位的疼痛等級映射
-  setPainLevel: React.Dispatch<React.SetStateAction<{ [key: string]: number }>>;  // 更新疼痛等級的狀態函數
-  MonthPain: { [key: string]: boolean };  // 每個部位是否在過去一年有疼痛
-  setMonthPain: React.Dispatch<React.SetStateAction<{ [key: string]: boolean }>>;  // 更新每年疼痛狀態的函數
-  WeekPain: { [key: string]: boolean };  // 每個部位是否在過去一週有疼痛
-  setWeekPain: React.Dispatch<React.SetStateAction<{ [key: string]: boolean }>>;  // 更新每週疼痛狀態的函數
+  currentPart: string; // 當前選擇的身體部位 ID
+  setCurrentPart: (part: string) => void; // 更新當前部位的函數
+  PainLevel: { [key: string]: number }; // 每個部位的疼痛等級映射
+  setPainLevel: React.Dispatch<React.SetStateAction<{ [key: string]: number }>>; // 更新疼痛等級的狀態函數
+  MonthPain: { [key: string]: boolean }; // 每個部位是否在過去一年有疼痛
+  setMonthPain: React.Dispatch<
+    React.SetStateAction<{ [key: string]: boolean }>
+  >; // 更新每年疼痛狀態的函數
+  WeekPain: { [key: string]: boolean }; // 每個部位是否在過去一週有疼痛
+  setWeekPain: React.Dispatch<React.SetStateAction<{ [key: string]: boolean }>>; // 更新每週疼痛狀態的函數
 }
-
 
 /**
  * DataFiller 組件
@@ -24,7 +25,7 @@ interface DataFillerProps {
 
 const DataFiller: React.FC<DataFillerProps> = (props) => {
   const [infoOpen, setInfoOpen] = useState(false);
-  
+
   // Weekvalue 狀態：用於追蹤該部位過去一週的疼痛狀態
   const [WeekValue, setWeekValue] = useState(false); // 更新 Weekvalue 狀態
   const onChangeWeek = (e: RadioChangeEvent) => {
@@ -39,48 +40,55 @@ const DataFiller: React.FC<DataFillerProps> = (props) => {
    */
 
   const onChangeMonth = (e: RadioChangeEvent) => {
-    setMonthValue(e.target.value);  // 更新 Monthvalue 狀態
+    setMonthValue(e.target.value); // 更新 Monthvalue 狀態
   };
 
   // value 狀態：記錄疼痛等級
   const [value, setValue] = useState(0);
   //處理疼痛等級滑塊值變更的函數
-  
+
   const onChangeValue = (value: number) => {
     setValue(value);
   };
 
-  
   const navigate = useNavigate();
   // 讀取 query parameter
   const location = useLocation();
   const queryParam = new URLSearchParams(location.search);
-  const pain_level = parseInt(queryParam.get('pain_level'), 10);
-  const month_pain = queryParam.get('month_pain') === 'true';
-  const week_pain = queryParam.get('week_pain') === 'true';
+  const pain_level = parseInt(queryParam.get("pain_level"), 10);
+  const month_pain = queryParam.get("month_pain") === "true";
+  const week_pain = queryParam.get("week_pain") === "true";
 
   useEffect(() => {
-
     // 用 query parameter 預填表單
-    if(props.currentPart !== '' && !isNaN(pain_level)){
+    if (props.currentPart !== "" && !isNaN(pain_level)) {
       setValue(pain_level);
       setMonthValue(month_pain);
       setWeekValue(week_pain);
-      navigate('/interact');
+      navigate("/interact");
     }
 
     // 若非初始值，預填上次輸入值
-    else if(props.currentPart !== '' && (
-        props.PainLevel[props.currentPart] !== 0 
-    ||  props.MonthPain[props.currentPart] !== false
-    ||  props.WeekPain[props.currentPart] !== false
-      ))
-    {
+    else if (
+      props.currentPart !== "" &&
+      (props.PainLevel[props.currentPart] !== 0 ||
+        props.MonthPain[props.currentPart] !== false ||
+        props.WeekPain[props.currentPart] !== false)
+    ) {
       setValue(props.PainLevel[props.currentPart]);
       setMonthValue(props.MonthPain[props.currentPart]);
       setWeekValue(props.WeekPain[props.currentPart]);
     }
-  }, [props.currentPart])
+  }, [
+    props.currentPart,
+    pain_level,
+    month_pain,
+    week_pain,
+    navigate,
+    props.PainLevel,
+    props.MonthPain,
+    props.WeekPain,
+  ]);
 
   const getPainLevelText = (value: number) => {
     if (0 <= value && value <= 1) return "沒有疼痛";
@@ -93,103 +101,125 @@ const DataFiller: React.FC<DataFillerProps> = (props) => {
   };
   // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
   return (
-      <Modal
-        title="填寫疼痛資料"
-        open={props.currentPart !== ''} // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
-        onOk={() => {
-          // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
-          props.MonthPain[props.currentPart] = MonthValue;
-          props.WeekPain[props.currentPart] = WeekValue;
-          props.PainLevel[props.currentPart] = value;
-          // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
-          props.setCurrentPart('');
-          setMonthValue(false);
-          setWeekValue(false);
-          setValue(0);
-        }}
-        onCancel={() => {
-          // 按下 Cancel 後清除當前選擇的部位與值
-          props.setCurrentPart(''); 
-          setMonthValue(false);
-          setWeekValue(false);
-          setValue(0);
-        }}
-        mask={false}// 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
-      > {/* 表單，用於填寫疼痛資料 */}
-        <Form>
-          {/* 疼痛等級滑塊，範圍從 0 到 10 */}
-          <Form.Item label="過去一年有無疼痛？">
-              <Slider 
-                onChange={onChangeValue} 
-                value={value} 
-                max={10} 
-                min={0}
-                marks={{
-                  0: "0",
-                  1: "1",
-                  2: "2",
-                  3: "3",
-                  4: "4",
-                  5: "5",
-                  6: "6",
-                  7: "7",
-                  8: "8",
-                  9: "9",
-                  10: "10"
-                }}
-              />
-            
-              <Typography.Text strong>疼痛程度：</Typography.Text>
-              <Typography.Text style={{
-                color:
-                  value <= 1 ? '#999999' : // 0-1 分：灰色
-                  value <= 3 ? 'green' : // 2-3 分：綠色
-                  value <= 5 ? '#FFB81C' : // 4-5 分：較深黃色
-                  value <= 7 ? '#ff8c00' : // 6-7 分：橙色
-                  value <= 9 ? 'red' : // 8-9 分：紅色
-                  'black' // 10 分：黑色
-              }}>
-                {getPainLevelText(value)} ({value}分)
-              </Typography.Text>
-            
-            <div style={{ textAlign: 'right' }}>
-              <Typography.Link onClick={() => setInfoOpen(true)}>疼痛分級說明</Typography.Link>
-            </div>
+    <Modal
+      title="填寫疼痛資料"
+      open={props.currentPart !== ""} // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
+      onOk={() => {
+        // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
+        props.MonthPain[props.currentPart] = MonthValue;
+        props.WeekPain[props.currentPart] = WeekValue;
+        props.PainLevel[props.currentPart] = value;
+        // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
+        props.setCurrentPart("");
+        setMonthValue(false);
+        setWeekValue(false);
+        setValue(0);
+      }}
+      onCancel={() => {
+        // 按下 Cancel 後清除當前選擇的部位與值
+        props.setCurrentPart("");
+        setMonthValue(false);
+        setWeekValue(false);
+        setValue(0);
+      }}
+      mask={false} // 組件的返回內容，使用 Modal 來顯示和填寫疼痛資料
+    >
+      {" "}
+      {/* 表單，用於填寫疼痛資料 */}
+      <Form>
+        {/* 疼痛等級滑塊，範圍從 0 到 10 */}
+        <Form.Item label="過去一年有無疼痛？">
+          <Slider
+            onChange={onChangeValue}
+            value={value}
+            max={10}
+            min={0}
+            marks={{
+              0: "0",
+              1: "1",
+              2: "2",
+              3: "3",
+              4: "4",
+              5: "5",
+              6: "6",
+              7: "7",
+              8: "8",
+              9: "9",
+              10: "10",
+            }}
+          />
 
-          </Form.Item>
-          {/* 是否在過去一年影響正常生活的 Radio 選項 */}
-          <Form.Item label="過去一年中,此部位的疼痛是否影響正常生活？">
-            <Radio.Group onChange={onChangeMonth} value={MonthValue}>
-              <Radio value={true}>Yes</Radio>
-              <Radio value={false}>No</Radio>
-            </Radio.Group>
-          </Form.Item>
-          {/* 是否在過去一星期影響正常生活的 Radio 選項 */}
-          <Form.Item label="過去一星期中，此部位是否還疼痛？">
-            <Radio.Group onChange={onChangeWeek} value={WeekValue}>
-              <Radio value={true}>Yes</Radio> {/* 是的，過去一週有疼痛 */}
-              <Radio value={false}>No</Radio> {/* 否，過去一週無疼痛 */}
-            </Radio.Group>
-          </Form.Item>
-        </Form>
-        {/* 疼痛分級說明視窗 */}
-        <Modal
-          open={infoOpen}
-          onCancel={() => setInfoOpen(false)}
-          footer={null}
-          title="疼痛分級說明"
-        >
-          <ul>
-            <li><strong>0-1 分</strong>：沒有疼痛</li>
-            <li><strong>2-3 分</strong>：輕微疼痛</li>
-            <li><strong>4-5 分</strong>：疼痛不舒服</li>
-            <li><strong>6-7 分</strong>：疼痛很困擾</li>
-            <li><strong>8-9 分</strong>：疼痛很嚴重</li>
-            <li><strong>10 分</strong> ：最痛</li>
-          </ul>
-        </Modal>
+          <Typography.Text strong>疼痛程度：</Typography.Text>
+          <Typography.Text
+            style={{
+              color:
+                value <= 1
+                  ? "#999999" // 0-1 分：灰色
+                  : value <= 3
+                    ? "green" // 2-3 分：綠色
+                    : value <= 5
+                      ? "#FFB81C" // 4-5 分：較深黃色
+                      : value <= 7
+                        ? "#ff8c00" // 6-7 分：橙色
+                        : value <= 9
+                          ? "red" // 8-9 分：紅色
+                          : "black", // 10 分：黑色
+            }}
+          >
+            {getPainLevelText(value)} ({value}分)
+          </Typography.Text>
+
+          <div style={{ textAlign: "right" }}>
+            <Typography.Link onClick={() => setInfoOpen(true)}>
+              疼痛分級說明
+            </Typography.Link>
+          </div>
+        </Form.Item>
+        {/* 是否在過去一年影響正常生活的 Radio 選項 */}
+        <Form.Item label="過去一年中,此部位的疼痛是否影響正常生活？">
+          <Radio.Group onChange={onChangeMonth} value={MonthValue}>
+            <Radio value={true}>Yes</Radio>
+            <Radio value={false}>No</Radio>
+          </Radio.Group>
+        </Form.Item>
+        {/* 是否在過去一星期影響正常生活的 Radio 選項 */}
+        <Form.Item label="過去一星期中，此部位是否還疼痛？">
+          <Radio.Group onChange={onChangeWeek} value={WeekValue}>
+            <Radio value={true}>Yes</Radio> {/* 是的，過去一週有疼痛 */}
+            <Radio value={false}>No</Radio> {/* 否，過去一週無疼痛 */}
+          </Radio.Group>
+        </Form.Item>
+      </Form>
+      {/* 疼痛分級說明視窗 */}
+      <Modal
+        open={infoOpen}
+        onCancel={() => setInfoOpen(false)}
+        footer={null}
+        title="疼痛分級說明"
+      >
+        <ul>
+          <li>
+            <strong>0-1 分</strong>：沒有疼痛
+          </li>
+          <li>
+            <strong>2-3 分</strong>：輕微疼痛
+          </li>
+          <li>
+            <strong>4-5 分</strong>：疼痛不舒服
+          </li>
+          <li>
+            <strong>6-7 分</strong>：疼痛很困擾
+          </li>
+          <li>
+            <strong>8-9 分</strong>：疼痛很嚴重
+          </li>
+          <li>
+            <strong>10 分</strong> ：最痛
+          </li>
+        </ul>
       </Modal>
+    </Modal>
   );
 };
 
-export default DataFiller;  // 導出 DataFiller 組件
+export default DataFiller; // 導出 DataFiller 組件
